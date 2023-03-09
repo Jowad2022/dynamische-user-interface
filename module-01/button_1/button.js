19fe93d430384773d556f6489dc70c16345bd63f
Jowad2022 <99059303@mydavinci.nl>
2023-03-09 11:15:23 +0100

    commit

---
+++ module-01/button_1/button.js
@@ -0,0 +1,4 @@
+const showImageButton = document.getElementById("show-image");
+	const myImage = document.getElementById("show-image"); 
+	showImageButton.addEventListener("click", () => { 
+		myImage.style.display = "block";});
