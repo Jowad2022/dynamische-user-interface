8fb65ee0120390eb500c85e8b098770ab491efc5
Jowad2022 <99059303@mydavinci.nl>
2023-03-28 09:11:45 +0200

    commit
    Signed-off-by: Jowad2022 <99059303@mydavinci.nl>

--- module-02/buttons_3/buttons.js
+++ module-02/buttons_3/buttons.js
@@ -1,17 +1,9 @@
 colorslist= ["red","purple","blue","black"];
-count = -1
-function buttons(){
-    for(let i = 0; i< 30; i++){
-        var div = document.createElement('button');
-        div.setAttribute("id", i)
-        div.setAttribute("onclick", i)
-        div.textContent = "click me"
-        container.appendChild(div)
-        div.addEventListener("click", function () {
-        onclick = count = count + 1; 
-        document.getElementById(i).style.backgroundColor = colorslist[count]
-        console.log(count)
-        }) 
-    }
+
+for(let i = 0; i< 30; i++){
+    var div = document.createElement('button');
+    div.setAttribute("id", i)
+    div.setAttribute("onclick", "colorchange("+i+")")
+    div.textContent = "click me"
+    container.appendChild(div)
 }
-buttons()
