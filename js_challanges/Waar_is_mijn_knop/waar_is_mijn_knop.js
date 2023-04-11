e4b0a983e66de3d738806f0331838c4fde07f3d1
Jowad2022 <99059303@mydavinci.nl>
2023-04-11 11:22:29 +0200

    commit

---
+++ js_challanges/Waar_is_mijn_knop/waar_is_mijn_knop.js
@@ -0,0 +1,16 @@
+function change() {
+    const btn = document.getElementById('btn');
+    document.getElementById("myheader").innerHTML = "welkom Jowad!";
+    if (document.getElementById("myheader").innerHTML = "welkom Jowad!")
+        div.setAttribute("onclick", "btn.remove()")
+}
+var div = document.createElement('button');
+    div.setAttribute("id", "btn")
+    div.setAttribute("class", "my_button")
+    div.setAttribute("onclick", "change()")
+    div.textContent = "Top Secret Stamp"
+    container.appendChild(div)
+    
+        
+    console.log
+
--- module-02/buttons_3/buttons.js
+++ module-02/buttons_3/buttons.js
@@ -1,17 +1,19 @@
-colorslist= ["red","purple","blue","black"];
+colorslist= ["green","red","purple","blue","black"];
 
 for(let i = 0; i< 30; i++){
     var div = document.createElement('button');
+    div.style.backgroundColor= colorslist[0]
     div.setAttribute("id", i)
     div.setAttribute("onclick", "colorchange("+i+")")
-    div.textContent = "click me"
+    div.textContent = i + " click me!"
     container.appendChild(div)
 }
 function colorchange(kleur){
-    colorindex = document.getElementById(kleur)
-    colorindex in indexof(colorslist)
-    if ((colorindex[1+1])==colorslist.length)
-        button.remove()
-    else
-        colorindex + 1
+    colorindex = document.getElementById(kleur).style.backgroundColor
+    nr=colorslist.indexOf(colorindex)
+    console.log(nr)
+    document.getElementById(kleur).style.backgroundColor = colorslist[nr+1]
+    if ((nr+1)==colorslist.length-1){
+        document.getElementById(kleur).remove()
+    }
 }
